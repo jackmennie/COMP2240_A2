1f3d62426fdf5cc9da2abe95ae8108e96c79879c
Jack Mennie <Jack.Mennie@uon.edu.au>
2020-10-06 21:07:46 +1100

    Completed P1

--- src/problem1/Bridge.java
+++ src/problem1/Bridge.java
@@ -1,26 +1,53 @@
+/**
+ * I don't ever wanna feel
+ * Like I did that day
+ * Take me to the place I love
+ * Take me all the way
+ * I don't ever wanna feel
+ * Like I did that day
+ * Take me to the place I love
+ * Take me all the way
+ *      - Red Hot Chili Peppers - Under the Bridge
+ * 
+ * The bridge contains a semaphore which locks itself 
+ * if it has detected load.
+ * 
+ * It takes 20 steps to cross the bridge, determined by
+ * the max length.
+ * 
+ * Holds statistical information (bridge crossings), which
+ * is the count of farmers that have crossed the bridge
+ * 
+ * Jack Mennie
+ * C3238004
+ */
 package src.problem1;
 
 import java.util.concurrent.Semaphore;
 
 public class Bridge {
-    private final int MAX_LOAD = 20;
-    private int currentLoad;
+    private final int LENGTH = 20;
+    private int bridgeCrossings;
 
     private Semaphore isDeadlocked = new Semaphore(1, true);
 
     public Bridge() {
-        currentLoad = 0;
+        bridgeCrossings = 0;
     }
 
-    public void incrementLoad() {
-        currentLoad++;
+    public void incrementCrossings() {
+        bridgeCrossings++;
     }
 
-    public int getMaxBridgeLoad() {
-        return MAX_LOAD;
+    public int getBridgeCrossings() {
+        return bridgeCrossings;
     }
 
-    public Semaphore isDeadlocked() {
+    public int getBridgeLength() {
+        return LENGTH;
+    }
+
+    public Semaphore lock() {
         return isDeadlocked;
     }
 
--- src/problem1/Problem1.java
+++ src/problem1/Problem1.java
@@ -1,19 +1,52 @@
+/**
+ * Problem 1: The New Zealand Bridge Dilemma 
+ * Jack Mennie
+ * C3238004
+ */
 package src.problem1;
 
-public class Problem1 implements Runnable {
+import java.util.ArrayList;
+
+public class Problem1 {
     private int northFarmersSize;
     private int southFarmersSize;
 
+    /**
+     * Initialises the problem by passing in the params and sets it
+     * 
+     * @param numberOfNorthFarmers
+     * @param numberOfSouthFarmers
+     */
     public void init(int numberOfNorthFarmers, int numberOfSouthFarmers) {
         northFarmersSize = numberOfNorthFarmers;
         southFarmersSize = numberOfSouthFarmers;
 
-        System.out.println(String.format("%-9s %-9s", northFarmersSize, southFarmersSize));
     }
 
-    @Override
+    /**
+     * Sets up the farmers, threads, and runs it
+     */
     public void run() {
-        System.out.println("Running P1");
+        Bridge bridge = new Bridge();
+
+        // Generate our farmer threads
+        ArrayList<Thread> farmers = new ArrayList<>();
+
+        for (int i = 0; i < northFarmersSize; i++) {
+            String id = "N_Farmer" + (i + 1);
+            Farmer farmer = new Farmer(id, "South", bridge);
+            farmers.add(new Thread(farmer));
+        }
+
+        for (int i = 0; i < southFarmersSize; i++) {
+            String id = "S_Farmer" + (i + 1);
+            Farmer farmer = new Farmer(id, "North", bridge);
+            farmers.add(new Thread(farmer));
+        }
+
+        // Start them threads
+        for (Thread farmer : farmers) {
+            farmer.start();
+        }
     }
-    
 }
